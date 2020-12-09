4aba638ab1bc30c1e2dd617dffc5ba139c97de77
Kotaro-Kitamura <kotarokitamura1983@gmail.com>
2020-12-09 09:21:57 +0900

    Add feature2

--- App.tsx
+++ App.tsx
@@ -3,11 +3,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    
-  );
-}
-
+jjj
+  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
